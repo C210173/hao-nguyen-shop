6bf7b0ee91d48d66ec3e3cfb75fc0c5600bc1414
C210173 <c210173@ccg.ac.jp>
2021-11-22 12:02:02 +0900

    update ngay 22/11

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,30 @@
-var slideIndex = 0;
-carousel();
+var slideIndex = 1;
+showSlides(slideIndex);
 
-function carousel() {
-var i;
-var x = document.getElementsByClassName("mySlides");
-for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
+function plusSlides(n) {
+  showSlides((slideIndex += n));
 }
-slideIndex++;
-if (slideIndex > x.length) {slideIndex = 1}
-x[slideIndex-1].style.display = "block";
-setTimeout(carousel, 5000); // Change image every 2 seconds
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
 }
